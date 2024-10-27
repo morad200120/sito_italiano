d7bbf5cdfcfcf27462ff04cc856ba7fc14c7a479
morad200120 <morad@x4brs.onmicrosoft.com>
2024-10-27 17:26:21 +0100

    aggiunto reindirizzamneto in base al dispositivo per il cambio del layout

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import user_agents
+
+app = Flask(__name__)
+app.secret_key = "s2f2h4*%!)81l#-nirpxe#*fd9-!+=&)0$ix=!8do%zot**z-p"
+
+@app.route("/")
+def ottieni_ua():
+    user_agent = request.headers.get("User-Agent")
+    ua = user_agents.parse(user_agent)
+
+    if ua.is_pc:
+        return redirect(url_for("desktop_page"))
+    else:
+        return redirect(url_for("mobile_page"))
+
+
+@app.route("/desktop_page", methods=["GET"])
+def desktop_page():
+    return render_template("desktop_index.html")
+
+@app.route("/mobile_page", methods=["GET"])
+def mobile_page():
+    return render_template("mobile_index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
