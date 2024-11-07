680a0dc1ac84ea1f9a5ecdcf87a49f59bd2dfa9d
morad200120 <morad@x4brs.onmicrosoft.com>
2024-11-07 21:35:09 +0100

    Siamo al punto di aver sistemato tutto dobbiamo creare il layout delle pagine mobile e quello del login della pagina di login desktop

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for
 import user_agents
 
 app = Flask(__name__)
 app.secret_key = "s2f2h4*%!)81l#-nirpxe#*fd9-!+=&)0$ix=!8do%zot**z-p"
 
-@app.route("/")
-def ottieni_ua():
+#-------------------------------------------------------------------------------
+
+def redirect_based_on_device(pc_route, mobile_route):
     user_agent = request.headers.get("User-Agent")
     ua = user_agents.parse(user_agent)
+    return redirect(url_for(pc_route if ua.is_pc else mobile_route))
 
-    if ua.is_pc:
-        return redirect(url_for("desktop_page"))
-    else:
-        return redirect(url_for("mobile_page"))
+#-------------------------------------------------------------------------------
 
+@app.route("/")
+def index():
+    return redirect_based_on_device("desktop_page", "mobile_page")
 
-@app.route("/desktop_page", methods=["GET"])
+@app.route("/aggiungi_articolo", methods=["GET"])
+def aggiungi_articolo():
+    return redirect_based_on_device("desktop_login", "mobile_login")
+
+#-------------------------------------------------------------------------------
+
+@app.route("/desktop_page")
 def desktop_page():
     return render_template("desktop_index.html")
 
-@app.route("/mobile_page", methods=["GET"])
+@app.route("/mobile_page")
 def mobile_page():
     return render_template("mobile_index.html")
 
+@app.route("/login_desktop")
+def desktop_login():
+    return render_template("desktop_login.html")
+
+@app.route("/login_mobile")
+def mobile_login():
+    return render_template("mobile_login.html")
+
+#-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- static/js/index.js
+++ static/js/index.js
@@ -2,5 +2,5 @@ console.log("file allegato")
 const button = document.getElementById("aggiungi_articoli");
 
 button.addEventListener("click", function() {
-    console.log("Pulsante cliccato")
+    
 })
