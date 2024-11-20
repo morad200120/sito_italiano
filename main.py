2c32c351771a2159135d6f88f579923eb337c131
morad200120 <morad@x4brs.onmicrosoft.com>
2024-11-20 01:17:05 +0100

    Aggiunto la funzionalita per accedere alla pagina del sito da dove si potranno aggiugere gli articoli

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import user_agents
 
 app = Flask(__name__)
 app.secret_key = "s2f2h4*%!)81l#-nirpxe#*fd9-!+=&)0$ix=!8do%zot**z-p"
+
+username = "admin"
+password = "admin"
 
 #-------------------------------------------------------------------------------
 
@@ -11,34 +14,56 @@ def redirect_based_on_device(pc_route, mobile_route):
     ua = user_agents.parse(user_agent)
     return redirect(url_for(pc_route if ua.is_pc else mobile_route))
 
+def login_required(f):
+    def wrap(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("index"))
+        return f(*args, **kwargs)
+    wrap.__name__ = f.__name__  # Mantiene il nome originale della funzione
+    return wrap
+
 #-------------------------------------------------------------------------------
 
 @app.route("/")
 def index():
+    error = session.pop("error", None)  # Recupera l'errore dalla sessione
     return redirect_based_on_device("desktop_page", "mobile_page")
 
-@app.route("/aggiungi_articolo", methods=["GET"])
+@app.route("/add_article", methods=["POST"])
 def aggiungi_articolo():
-    return redirect_based_on_device("desktop_login", "mobile_login")
+    username_inserito = request.form.get("username") 
+    password_inserita = request.form.get("password")
+
+    if username == username_inserito and password == password_inserita:
+        session["logged_in"] = True
+    else:
+        session["error"] = "Credenziali non corrette, riprova"
+        return redirect(url_for("index"))
+
+    return redirect_based_on_device("add_article_desktop_page", "add_article_mobile_page")
 
 #-------------------------------------------------------------------------------
 
 @app.route("/desktop_page")
 def desktop_page():
-    return render_template("desktop_index.html")
+    error = session.pop("error", None)  # Recupera il messaggio di errore
+    return render_template("desktop_index.html", error=error)
 
 @app.route("/mobile_page")
 def mobile_page():
-    return render_template("mobile_index.html")
+    error = session.pop("error", None)  # Recupera il messaggio di errore
+    return render_template("mobile_index.html", error=error)
 
-@app.route("/desktop_login")
-def desktop_login():
-    return render_template("desktop_login.html")
+@app.route("/desktop_add_article")
+@login_required
+def add_article_desktop_page():
+    return render_template("desktop_add_article.html")
 
-@app.route("/mobile_login")
-def mobile_login():
-    return render_template("mobile_login.html")
+@app.route("/mobile_add_article")
+@login_required
+def add_article_mobile_page():
+    return render_template("mobile_add_article.html")
 
 #-------------------------------------------------------------------------------
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
